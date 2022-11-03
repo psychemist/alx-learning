3bb90c09dce549df33882d931ab7cd61c28161fe
psych0wh1z <ikeagudike@gmail.com>
2022-11-03 23:57:34 +0100

    Add task 6 files

---
+++ 6-pointers/task6.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char spec[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	i = 0;
+		
+	while (s[i])
+	{
+		j = 0;
+		while (spec[j])
+		{
+			if (s[i] == spec[j])
+			{
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				{
+					s[i + 1] -= 32;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return s;
+}
+
+int main(void)
+{
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
