8e901bf7171611c0bb01912c5c794264cf17b44f
psych0wh1z <ikeagudike@gmail.com>
2022-11-04 03:51:21 +0100

    Correct logic flow

--- 6-pointers/task3.c
+++ 6-pointers/task3.c
@@ -3,21 +3,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, val1, val2;
-	val1 = val2 = 0;
+	int i;
 
-	for (i = 0; s1[i]; i++)
-		val1 += s1[i];
-
-	for (i = 0; s2[i]; i++)
-		val2 += s2[i];
-
-	if (val1 > val2)
-		return 15;
-	else if (val2 > val1)
-		return -15;
-	else
-		return 0;
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	
+	return (0);
 }
 
 int main(void)
