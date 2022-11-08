976f23fa23a26e5239c6a46331a711159b4f3168
psych0wh1z <ikeagudike@gmail.com>
2022-11-08 02:11:22 +0100

    Add task5 files

---
+++ 8-recursion/task5.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt_recursion(int n);
+
+int root(int base, int num)
+{
+	int next = (base + num / base) / 2;
+
+	if (base * base <= num && (base + 1) * (base + 1) > num)
+	{
+		if (num % base == 0 && num / base == base)
+			return (base);
+		else
+			return (-1);
+	}
+	else
+		return root(next, num);
+
+/*	if (base * base == num)
+	{
+		return (base);
+	}
+	if (base 
+	*/
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	return root(1, n);
+}
+
+int main(void)
+{
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
+    return (0);
+}
