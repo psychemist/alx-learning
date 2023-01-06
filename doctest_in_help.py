a74029601017fe7ae2af68cfeb760bc949a3d7fb
psych0wh1z <ikeagudike@gmail.com>
2023-01-06 11:48:39 +0100

    Add unittest files

--- doctest_in_help.py
+++
@@ -1,6 +0,0 @@
-# doctest_in_help.py
-
-def my_function(a, b):
-    """Returns a*b
-    """
-    return a * b
--- doctest_private_tests_external.py
+++
@@ -1,6 +0,0 @@
-# doctest_private_tests_external.py
-"""External tests associated with doctest_private_tests.py.
-
->>> my_function(['A', 'B', 'C'], 2)
-['A', 'B', 'C', 'A', 'B', 'C']
-"""
