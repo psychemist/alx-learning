19051ead072c9e4b1e415759aac807eae3f4a93e
psych0wh1z <ikeagudike@gmail.com>
2023-02-05 05:16:56 +0100

    Add turtle graphics directory

---
+++ turtle_graphics/geode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import turtle
+
+tort = turtle.Turtle()
+
+tort.forward(100)
+tort.left(90)
+tort.forward(100)
+tort.left(90)
+tort.forward(100)
+tort.left(90)
+tort.forward(100)
+
+
+
+
+turtle.done()
