4dbe2aaa2a119682c60a5d50f403a834a90aab19
psych0wh1z <ikeagudike@gmail.com>
2023-03-06 07:56:01 +0100

    Fix code logic

--- 2-functions/task12.c
+++ 2-functions/task12.c
@@ -4,49 +4,46 @@ void print_times_table(int n)
 {
 	int i, j, times;
 
-	if (n > 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			times = i * j;
+
+			if (j != 0)
 			{
-				times = i * j;
-
+				_putchar(',');
+				_putchar(' ');
+				if (times < 100)
+					_putchar(' ');
 				if (times < 10)
-				{
-					_putchar(times + '0');
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-				if (times > 9)
-				{
-					if (times < 100)
-					{
-						_putchar(times / 10 + '0');
-						_putchar(times % 10 + '0');
-					}
-
-					if (times >= 100)
-					{
-						_putchar(times / 100 + '0');
-						_putchar(times % 100 + '0');
-					}
-
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
+					_putchar(' ');
 			}
-			_putchar('\n');
+
+			if (times < 10)
+			{
+				_putchar(times + '0');
+			}
+
+			if (times > 9)
+			{
+				if (times >= 100)
+				{
+					_putchar(times / 100 + '0');
+					times = times - 100;
+				}
+
+				_putchar(times / 10 + '0');
+				_putchar(times % 10 + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
+
 
 int main(void)
 {
